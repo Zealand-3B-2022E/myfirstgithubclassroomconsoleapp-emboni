954d17ade48dec84d3c56f5d66b0ada952439bbf
emboni <93706027+emboni@users.noreply.github.com>
2022-09-05 12:52:33 +0200

    New commit 5. sep

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+        private string name = "Team Building";
+        private int number = 45;
+
+        
+        private int myAge;
+
+        public int MyProperty
+        {
+            get { return myAge; }
+            set { myAge = value; }
+        }
+
+    }
+
+    
+    
+}
